abe583bfd743eda532ed380ff70a95fc3e378613
Dan <dan.arias@auth0.com>
2019-12-19 09:09:40 -0500

    Create API endpoints with error handling

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
 
 dotenv.config();
 
@@ -28,6 +30,9 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/items", itemsRouter);
+// Add Error Handler
+app.use(errorHandler);
 
 /**
  * Server Activation
---
+++ src/items/items.router.ts
@@ -0,0 +1,86 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+import express, { Request, Response } from "express";
+import * as ItemService from "./items.service";
+import { Item } from "./item.interface";
+import { Items } from "./items.interface";
+
+/**
+ * Router Definition
+ */
+
+export const itemsRouter = express.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET items/
+
+itemsRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const items: Items = await ItemService.findAll();
+
+    res.status(200).send(items);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+// GET items/:id
+
+itemsRouter.get("/:id", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const item: Item = await ItemService.find(id);
+
+    res.status(200).send(item);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+
+// POST items/
+
+itemsRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const item: Item = req.body.item;
+
+    await ItemService.create(item);
+
+    res.sendStatus(201);
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+// PUT items/
+
+itemsRouter.put("/", async (req: Request, res: Response) => {
+  try {
+    const item: Item = req.body.item;
+
+    await ItemService.update(item);
+
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// DELETE items/:id
+
+itemsRouter.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id, 10);
+    await ItemService.remove(id);
+
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
